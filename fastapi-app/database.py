8983f1c0f6239a7eb0bba8caee4f26e61dc3bf52
glyzaaa <apusglyzamae@gmail.com>
2025-04-15 21:40:16 +0800

    Add files via upload

---
+++ fastapi-app/database.py
@@ -0,0 +1,10 @@
+# database.py
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+database_url = os.environ.get("DATABASE_URL")
+
+engine = create_engine(database_url, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
